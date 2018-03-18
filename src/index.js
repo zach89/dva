7cef871584db5a7f0e74766b7709bfb07081fa27
root <zach88@163.com>
2018-03-18 18:47:30 +0800

    first

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import dva from 'dva';
+import { message } from 'antd';
+import createHistory from 'history/createBrowserHistory';
+import createLoading from 'dva-loading';
+import './index.css';
+
+// 1. Initialize
+const app = dva({
+    history: createHistory(),
+    initialState:{
+        products:[
+            { name: 'dva', id: 1 },
+            { name: 'antd', id: 2 },
+        ]
+    },
+    onError(e) {
+        message.error(e.message, /* duration */3);
+    },
+});
+app.use(createLoading());
+
+// 2. Plugins
+// app.use({});
+
+// 3. Model
+// app.model(require('./models/example').default);
+app.model(require('./models/products').default);
+app.model(require('./models/users').default);
+
+// 4. Router
+app.router(require('./router').default);
+
+// 5. Start
+app.start('#root');
